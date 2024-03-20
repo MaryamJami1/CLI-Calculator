8efd294cb1eca78a81e1b17e7305860322bd2c6c
MaryamJami1 <maryamchouhan373@gmail.com>
2024-03-20 21:27:43 +0500

    simple calculator

---
+++ index.ts
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// asking questions to users through inquirer
+
+let answers = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one operator to perform operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Multiplication", "Subtraction", "Division"]
+    }
+]);
+
+
+// conditional statement
+
+if (answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber)
+}
+
+else if (answers.operator === "Subtraction") {
+    console.log(answers.firstNumber - answers.secondNumber)
+}
+
+
+else if (answers.operator === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber)
+}
+
+else if (answers.operator === "Division") {
+    console.log(answers.firstNumber / answers.secondNumber)
+}
+
+else {
+    console.log("invalid input")
+}
